e9396f26366a664ff23268c8c3b39de6b0ee6621
Jullian Abinsay <jullianabinsay@gmail.com>
2023-09-18 15:17:25 -0400

    Adds and Updates Meet Us Section

--- src/ui/modules/MeetUs.js
+++ src/ui/modules/MeetUs.js
@@ -1,14 +1,14 @@
-function MeetOurTechs() {
+function MeetUs() {
     return(
         <section className="best-doctors section-gray pt100 pb100">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 col-md-offset-2">
                             <div className="section-title text-center mb40"> <img src="img/icons/icon-service-title.png" alt="" />
-                                <h2>Meet Our <span className="color-blue">Doctors</span></h2>
-                                <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain
+                                <h2>Meet Our <span className="color-blue">Staff</span></h2>
+                                {/* <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain
                                     was born and will give you a complete account of the system, and expound the actual
-                                    teachings</p>
+                                    teachings</p> */}
                             </div>
                         </div>
                     </div>
@@ -17,13 +17,14 @@ function MeetOurTechs() {
                             <div className="bestdoctor-box-v2 section-light"> <img src="img/doctors/doctorsv2-1.jpg" alt=""
                                     className="img-full" />
                                 <div className="bestdoctor-info text-center"> <a href="doctor-profile.html">
-                                        <h6>Alex Garcia Perez</h6>
+                                        <h6>Steve Pankey</h6>
                                     </a>
-                                    <p>Anethesiologist</p> <a className="see-detail" href="doctor-profile.html">See Details</a>
+                                    <p>Owner</p> 
+                                    <a className="see-detail" href="doctor-profile.html">See Details</a>
                                 </div>
                                 <div className="best-doctor-footer section-blue">
-                                    <div className="bestd-list-item">Experience<br/><span>5 Years</span></div>
-                                    <div className="bestd-list-item">Office<br/><span>Office 112, Hall C</span></div>
+                                    <div className="bestd-list-item">Experience<br/><span>25 Years</span></div>
+                                    <div className="bestd-list-item">Department<br/><span>Admin</span></div>
                                 </div>
                             </div>
                         </div>
@@ -31,13 +32,14 @@ function MeetOurTechs() {
                             <div className="bestdoctor-box-v2 section-light"> <img src="img/doctors/doctorsv2-2.jpg" alt=""
                                     className="img-full" />
                                 <div className="bestdoctor-info text-center"> <a href="doctor-profile.html">
-                                        <h6>Robert Brown</h6>
+                                        <h6>Kim Pankey</h6>
                                     </a>
-                                    <p>Gynecologist</p> <a className="see-detail" href="doctor-profile.html">See Details</a>
+                                    <p>Owner & Technician</p> 
+                                    {/* <a className="see-detail" href="doctor-profile.html">See Details</a> */}
                                 </div>
                                 <div className="best-doctor-footer section-blue">
-                                    <div className="bestd-list-item">Experience<br/><span>5 Years</span></div>
-                                    <div className="bestd-list-item">Office<br/><span>Office 112, Hall C</span></div>
+                                    <div className="bestd-list-item">Experience<br/><span>25 Years</span></div>
+                                    <div className="bestd-list-item">Department<br/><span>Admin, Dispatch</span></div>
                                 </div>
                             </div>
                         </div>
@@ -45,22 +47,23 @@ function MeetOurTechs() {
                             <div className="bestdoctor-box-v2 section-light"> <img src="img/doctors/doctorsv2-3.jpg" alt=""
                                     className="img-full" />
                                 <div className="bestdoctor-info text-center"> <a href="doctor-profile.html">
-                                        <h6>ANNA SMITH</h6>
+                                        <h6>ROSANNA SMITH</h6>
                                     </a>
-                                    <p>Nurse</p> <a className="see-detail" href="doctor-profile.html">See Details</a>
+                                    <p>Owner & Technician</p> 
+                                    {/* <a className="see-detail" href="doctor-profile.html">See Details</a> */}
                                 </div>
                                 <div className="best-doctor-footer section-blue">
-                                    <div className="bestd-list-item">Experience<br/><span>5 Years</span></div>
-                                    <div className="bestd-list-item">Office<br/><span>Office 112, Hall C</span></div>
+                                    <div className="bestd-list-item">Experience<br/><span>25 Years</span></div>
+                                    <div className="bestd-list-item">Department<br/><span>Admin, Dispatch</span></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="text-center clearfix mt50"> <a href="doctors.html" className="primary-btn">View All Doctors</a>
+                    <div className="text-center clearfix mt50"> <a href="/staff" className="primary-btn">View All Staff</a>
                     </div>
                 </div>
             </section>
     )
 }
 
-export default MeetOurTechs
+export default MeetUs
--- src/ui/pages/Home.js
+++ src/ui/pages/Home.js
@@ -6,12 +6,12 @@ import Services from '../modules/Services';
 import Contact from '../modules/Contact';
 import Footer from '../modules/Footer';
 import ScrollUpButton from '../modules/ScrollUpButton';
-import MeetOurTechs from '../modules/MeetOurTechs';
 // import LatestNews from '../modules/LatestNews';
 import Preloader from '../modules/Preloader';
 import About from '../modules/About';
 import OurCoverage from '../modules/OurCoverage';
 import HomeSection from '../modules/HomeSection';
+import MeetUs from '../modules/MeetUs';
 
 function Home() {
 
@@ -43,8 +43,8 @@ function Home() {
             
             <About/>
 
-            <MeetOurTechs/>
-
+            <MeetUs/>
+            
             {/* <LatestNews/> */}
             
             <Contact/>
